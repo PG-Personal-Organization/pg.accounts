ae97ef4b4c7b9be7c6b946fc721535bcb9a8dfe9
Patryk Grzywacz <patrykg198@gmail.com>
2025-08-28 20:33:35 +0200

    refactor repo management

--- pg.accounts.infrastructure-spring/src/main/java/pg/accounts/infrastructure/spring/configuration/AccountsCacheConfiguration.java
+++ pg.accounts.infrastructure-spring/src/main/java/pg/accounts/infrastructure/spring/configuration/AccountsCacheConfiguration.java
@@ -1,31 +1,9 @@
 package pg.accounts.infrastructure.spring.configuration;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @EnableCaching
 @Configuration
 public class AccountsCacheConfiguration {
-    private static final String ACCOUNTS_CACHE = "accountsCache";
-
-    @Bean
-    public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager(ACCOUNTS_CACHE);
-    }
-
-    /**
-     * Users cache cache.
-     *
-     * @return the cache
-     */
-    @Bean
-    @Qualifier(ACCOUNTS_CACHE)
-    public Cache accountsCache() {
-        return cacheManager().getCache(ACCOUNTS_CACHE);
-    }
 }
